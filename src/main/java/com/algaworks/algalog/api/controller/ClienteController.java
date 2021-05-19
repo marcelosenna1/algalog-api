294d0d4dcb7195e5ffe91de06de42c34589f6ad2
Marcelo Sena <marcelomarcos2@gmail.com>
2021-05-19 16:12:32 -0300

    Usando Jakarta Persistence JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,13 @@ import com.algaworks.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 
-		var c1 = new Cliente(1L,"Marcelo1","75 991252901","marcelomarcos2@gmail.com");
-		var c2 = new Cliente(1L,"Zeus","75 991252901","zeus2@gmail.com");
-		
-
-		return Arrays.asList(c1, c2);
+	return manager.createQuery("from Cliente",Cliente.class).getResultList();
 	}
 }
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,17 +1,32 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Cliente  {
-
+@Entity
+public class Cliente {
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	
 	private String nome;
 	private String email;
+	@Column(name = "fone")
 	private String telefone;
 
 }
