f223e1749f11b7f628eec487b360ad09748914e7
Marcelo Sena <marcelomarcos2@gmail.com>
2021-05-19 23:31:10 -0300

    Adicionando Bean Validation

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -5,6 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -17,15 +22,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 public class Cliente {
-	
+
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	
+
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
+
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+
+	@NotBlank
+	@Size(max = 20)
 	@Column(name = "fone")
 	private String telefone;
 
