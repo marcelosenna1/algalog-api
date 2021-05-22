6cdf08f13a3c86f3752678b78cfdb7b801be4088
Marcelo Sena <marcelomarcos2@gmail.com>
2021-05-22 14:33:28 -0300

    Implementando entregas

---
+++ src/main/java/com/algaworks/algalog/domain/model/service/SolicitacaoEntregaService.java
@@ -0,0 +1,36 @@
+package com.algaworks.algalog.domain.model.service;
+
+import java.time.LocalDateTime;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algalog.domain.exception.NegocioException;
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.model.Entrega;
+import com.algaworks.algalog.domain.model.StatusEntrega;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+import com.algaworks.algalog.domain.repository.EntregaRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class SolicitacaoEntregaService {
+
+	private EntregaRepository entregaRepository;
+	private CatalogoClienteService catalogoClienteService;
+
+	@Transactional
+	public Entrega solicitar(Entrega entrega) {
+		Cliente cliente = catalogoClienteService.buscar(entrega.getCliente().getId());
+
+		entrega.setCliente(cliente);
+		entrega.setStatus(StatusEntrega.PENDENTE);
+		entrega.setDataPedido(LocalDateTime.now());
+
+		return entregaRepository.save(entrega);
+	}
+
+}
